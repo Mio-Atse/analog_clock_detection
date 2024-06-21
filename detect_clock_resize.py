0ff524a672bd8dcf0bab78f27094a672cff36f98
Samet Dönmez <123257898+Mio-Atse@users.noreply.github.com>
2024-06-21 21:36:35 +0300

    Update detect_clock_resize.py

--- detect_clock_resize.py
+++ detect_clock_resize.py
@@ -2,45 +2,45 @@ import os
 import torch
 from PIL import Image
 
-# Model yükleniyor
+# Loading the model
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
-# Klasör yolları
+# Paths for folders
 image_dir = 'images'
-fixed_image_dir = 'imaged_cropped_fixed'  # Yeni oluşturulan klasör adı
+fixed_image_dir = 'cropped_resized_images'  # New folder name for cropped and resized images
 
-# Kırpılan ve boyutlandırılan resimlerin kaydedileceği klasör yoksa oluşturuluyor
+# Creating the folder if it doesn't exist for cropped and resized images
 if not os.path.exists(fixed_image_dir):
     os.makedirs(fixed_image_dir)
 
-# 'images' klasöründeki tüm resim dosyalarını al
+# Get all image files in the 'images' folder
 image_files = [f for f in os.listdir(image_dir) if f.lower().endswith(('png', 'jpg', 'jpeg'))]
 
 for image_file in image_files:
     image_path = os.path.join(image_dir, image_file)
     
-    # Resmi aç
+    # Open the image
     with Image.open(image_path) as img:
         results = model(img)
         detections = results.pandas().xyxy[0]
 
         if not detections.empty:
-            # İlk tespiti alın
+            # Get the first detection
             xmin, ymin, xmax, ymax = int(detections.iloc[0]['xmin']), int(detections.iloc[0]['ymin']), int(detections.iloc[0]['xmax']), int(detections.iloc[0]['ymax'])
 
-            # Nesne kırpılıyor
+            # Crop the object
             cropped_image = img.crop((xmin, ymin, xmax, ymax))
 
-            # Boyutlandırma ve kaydetme işlemi
+            # Resize and save
             cropped_image.thumbnail((800, 800))
             fixed_image_path = os.path.join(fixed_image_dir, image_file)
             cropped_image.save(fixed_image_path)
             print(f"Resized and saved: {fixed_image_path}")
         else:
-            # Nesne tespit edilemezse orijinal resmi boyutlandır ve kaydet
+            # If no object is detected, resize and save the original image
             img.thumbnail((800, 800))
             fixed_image_path = os.path.join(fixed_image_dir, image_file)
             img.save(fixed_image_path)
             print(f"Resized and saved: {fixed_image_path}")
 
-print("Boyutlandırma ve kaydetme işlemi tamamlandı.")
+print("Resize and save operation completed.")
